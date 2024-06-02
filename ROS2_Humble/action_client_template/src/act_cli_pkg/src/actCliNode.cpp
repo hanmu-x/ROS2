da3e0bd0b7b0b74a659d2a22c74aa5213d8d4160
yingjie_wang <2778809626@qq.com>
2024-06-02 14:41:55 +0800

    更新action

--- ROS2_Humble/action_client_template/src/act_cli_pkg/src/actCliNode.cpp
+++ ROS2_Humble/action_client_template/src/act_cli_pkg/src/actCliNode.cpp
@@ -1,132 +1,108 @@
 #include <functional>
 #include <future>
 #include <memory>
-#include <string>
-#include <sstream>
-
-#include "action_pkg/action/fibonacci.hpp"
 
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
-#include "rclcpp_components/register_node_macro.hpp"
+#include "action_pkg/action/rotate.hpp"
 
-#include "act_cli_pkg/visibility_control.h"
+using Rotate = action_pkg::action::Rotate;
+using GoalHandleRotate = rclcpp_action::ClientGoalHandle<Rotate>;
 
-namespace action_tutorials_cpp
+class RotateActionClient : public rclcpp::Node
 {
-  class FibonacciActionClient : public rclcpp::Node
+public:
+  explicit RotateActionClient(const rclcpp::NodeOptions &options) : Node("rotate_action_client", options)
   {
-  public:
-    using Fibonacci = action_pkg::action::Fibonacci;
-    using GoalHandleFibonacci = rclcpp_action::ClientGoalHandle<Fibonacci>;
-
-    explicit FibonacciActionClient(const rclcpp::NodeOptions &options)
-        : Node("fibonacci_action_client", options)
+    // 函数创建了一个动作客户端，指定了节点实例指针 this 和动作服务器的名称 "rotate"
+    client_ptr_ = rclcpp_action::create_client<Rotate>(this, "rotate");
+    // 循环等待动作服务器上线，直到 wait_for_action_server 返回 true，表示服务器已经上线。
+    while (!client_ptr_->wait_for_action_server(std::chrono::seconds(1)))
     {
-      this->client_ptr_ = rclcpp_action::create_client<Fibonacci>(
-          this,
-          "fibonacci");
-
-      this->timer_ = this->create_wall_timer(
-          std::chrono::milliseconds(500),
-          std::bind(&FibonacciActionClient::send_goal, this));
+      RCLCPP_INFO(get_logger(), "Waiting for action server to be up...");
     }
+  }
 
-    void send_goal()
+  // 用于向动作服务器发送旋转目标
+  void send_goal(int degrees)
+  {
+    auto goal_msg = Rotate::Goal();
+    goal_msg.goal = degrees;
+
+    // using namespace std::placeholders;
+    // goal_response_callback 用于处理目标响应
+    auto send_goal_options = rclcpp_action::Client<Rotate>::SendGoalOptions();
+    send_goal_options.goal_response_callback = std::bind(&RotateActionClient::goal_response_callback, this, std::placeholders::_1);
+    // feedback_callback 用于处理反馈消息
+    send_goal_options.feedback_callback = std::bind(&RotateActionClient::feedback_callback, this, std::placeholders::_1, std::placeholders::_2);
+    // result_callback 用于处理目标完成后的结果
+    send_goal_options.result_callback = std::bind(&RotateActionClient::result_callback, this, std::placeholders::_1);
+    // 调用客户端的 async_send_goal (异步)函数发送目标，并将返回的 std::shared_future 对象存储在 goal_handle_future_ 成员中
+    goal_handle_future_ = client_ptr_->async_send_goal(goal_msg, send_goal_options);
+  }
+
+  // wait_for_result 函数用于等待目标完成。它调用 goal_handle_future_ 对象的 wait 函数，阻塞当前线程直到目标完成
+  void wait_for_result()
+  {
+    goal_handle_future_.wait();
+  }
+
+private:
+  rclcpp_action::Client<Rotate>::SharedPtr client_ptr_;                                       // 处理action的客户端 的智能指针
+  std::shared_future<rclcpp_action::ClientGoalHandle<Rotate>::SharedPtr> goal_handle_future_; // 存储未来获取的action目标句柄
+
+  // 当action目标被服务器接受或拒绝时被调用
+  void goal_response_callback(rclcpp_action::ClientGoalHandle<Rotate>::SharedPtr goal_handle)
+  {
+    if (!goal_handle)
     {
-      using namespace std::placeholders;
-
-      this->timer_->cancel();
-
-      if (!this->client_ptr_->wait_for_action_server())
-      {
-        RCLCPP_ERROR(this->get_logger(), "Action server not available after waiting");
-        rclcpp::shutdown();
-      }
-
-      auto goal_msg = Fibonacci::Goal();
-      goal_msg.order = 10;
-
-      RCLCPP_INFO(this->get_logger(), "Sending goal");
-
-      auto send_goal_options = rclcpp_action::Client<Fibonacci>::SendGoalOptions();
-      send_goal_options.goal_response_callback =
-          std::bind(&FibonacciActionClient::goal_response_callback, this, _1);
-      send_goal_options.feedback_callback =
-          std::bind(&FibonacciActionClient::feedback_callback, this, _1, _2);
-      send_goal_options.result_callback =
-          std::bind(&FibonacciActionClient::result_callback, this, _1);
-      this->client_ptr_->async_send_goal(goal_msg, send_goal_options);
+      RCLCPP_ERROR(get_logger(), "Goal was rejected by server");
     }
-
-  private:
-    rclcpp_action::Client<Fibonacci>::SharedPtr client_ptr_;
-    rclcpp::TimerBase::SharedPtr timer_;
-
-    void goal_response_callback(const GoalHandleFibonacci::SharedPtr &goal_handle)
+    else
     {
-      if (!goal_handle)
-      {
-        RCLCPP_ERROR(this->get_logger(), "Goal was rejected by server");
-      }
-      else
-      {
-        RCLCPP_INFO(this->get_logger(), "Goal accepted by server, waiting for result");
-      }
+      RCLCPP_INFO(get_logger(), "Goal accepted by server, waiting for result");
     }
-
-    void feedback_callback(
-        GoalHandleFibonacci::SharedPtr,
-        const std::shared_ptr<const Fibonacci::Feedback> feedback)
+  }
+  // 反馈回调函数，用于接收action执行过程中的反馈信息
+  void feedback_callback(GoalHandleRotate::SharedPtr, const std::shared_ptr<const Rotate::Feedback> feedback)
+  {
+    RCLCPP_INFO(get_logger(), "Current rotation angle: %d degrees", feedback->feedback);
+  }
+  // 结果回调函数，用于处理action执行结束后的结果
+  void result_callback(const rclcpp_action::ClientGoalHandle<Rotate>::WrappedResult &result)
+  {
+    switch (result.code)
     {
-      std::stringstream ss;
-      ss << "Next number in sequence received: ";
-      for (auto number : feedback->partial_sequence)
-      {
-        ss << number << " ";
-      }
-      RCLCPP_INFO(this->get_logger(), ss.str().c_str());
+    case rclcpp_action::ResultCode::SUCCEEDED:
+      printf("client Goal succeeded with final angle: %d degrees \n", result.result->result);
+      RCLCPP_INFO(get_logger(), "client Goal succeeded with final angle: %d degrees", result.result->result);
+      break;
+    case rclcpp_action::ResultCode::ABORTED:
+      RCLCPP_ERROR(get_logger(), "Goal was aborted");
+      return;
+    case rclcpp_action::ResultCode::CANCELED:
+      RCLCPP_ERROR(get_logger(), "Goal was canceled");
+      return;
+    default:
+      RCLCPP_ERROR(get_logger(), "Unknown result code");
+      return;
     }
-
-    void result_callback(const GoalHandleFibonacci::WrappedResult &result)
-    {
-      switch (result.code)
-      {
-      case rclcpp_action::ResultCode::SUCCEEDED:
-        break;
-      case rclcpp_action::ResultCode::ABORTED:
-        RCLCPP_ERROR(this->get_logger(), "Goal was aborted");
-        return;
-      case rclcpp_action::ResultCode::CANCELED:
-        RCLCPP_ERROR(this->get_logger(), "Goal was canceled");
-        return;
-      default:
-        RCLCPP_ERROR(this->get_logger(), "Unknown result code");
-        return;
-      }
-      std::stringstream ss;
-      ss << "Result received: ";
-      for (auto number : result.result->sequence)
-      {
-        ss << number << " ";
-      }
-      RCLCPP_INFO(this->get_logger(), ss.str().c_str());
-      rclcpp::shutdown();
-    }
-  }; // class FibonacciActionClient
-
-} // namespace action_tutorials_cpp
-
-RCLCPP_COMPONENTS_REGISTER_NODE(action_tutorials_cpp::FibonacciActionClient)
+  }
+};
 
 int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
-  // auto node = std::make_shared<action_tutorials_cpp::FibonacciActionClient>();
-  auto node_options = rclcpp::NodeOptions(); // 可以根据需要配置NodeOptions
-  auto node = std::make_shared<action_tutorials_cpp::FibonacciActionClient>(node_options);
-  rclcpp::spin(node);
-  rclcpp::shutdown();
 
+  auto options = rclcpp::NodeOptions().allow_undeclared_parameters(true);
+  auto client = std::make_shared<RotateActionClient>(options);
+
+  // 请求旋转270度
+  client->send_goal(270);
+
+  // 使用新的公共方法等待结果
+  client->wait_for_result();
+
+  rclcpp::shutdown();
   return 0;
 }
--- ROS2_Humble/action_server_template/src/action_proj/src/actProjNode.cpp
+++ ROS2_Humble/action_server_template/src/action_proj/src/actProjNode.cpp
@@ -1,113 +1,107 @@
 #include <functional>
 #include <memory>
+#include <chrono>
 #include <thread>
 
-#include "action_pkg/action/fibonacci.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
-#include "rclcpp_components/register_node_macro.hpp"
+#include "action_pkg/action/rotate.hpp"
 
-#include "action_proj/visibility_control.h"
+using Rotate = action_pkg::action::Rotate;
+using GoalHandleRotate = rclcpp_action::ServerGoalHandle<Rotate>;
 
-namespace action_tutorials_cpp
+class RotateActionServer : public rclcpp::Node
 {
-  class FibonacciActionServer : public rclcpp::Node
-  {
-  public:
-    using Fibonacci = action_pkg::action::Fibonacci;
-    using GoalHandleFibonacci = rclcpp_action::ServerGoalHandle<Fibonacci>;
-
-    ACTION_TUTORIALS_CPP_PUBLIC
-    explicit FibonacciActionServer(const rclcpp::NodeOptions &options = rclcpp::NodeOptions())
-        : Node("fibonacci_action_server", options)
+public:
+    // 初始化节点名为"rotate_action_server"
+    RotateActionServer() : Node("rotate_action_server")
     {
-      using namespace std::placeholders;
-
-      this->action_server_ = rclcpp_action::create_server<Fibonacci>(
-          this,
-          "fibonacci",
-          std::bind(&FibonacciActionServer::handle_goal, this, _1, _2),
-          std::bind(&FibonacciActionServer::handle_cancel, this, _1),
-          std::bind(&FibonacciActionServer::handle_accepted, this, _1));
+        // using namespace std::placeholders;
+        // 绑定处理目标、取消请求和接受目标的回调函数。
+        action_server_ = rclcpp_action::create_server<Rotate>(
+            this,
+            "rotate",
+            std::bind(&RotateActionServer::handle_goal, this, std::placeholders::_1, std::placeholders::_2),
+            std::bind(&RotateActionServer::handle_cancel, this, std::placeholders::_1),
+            std::bind(&RotateActionServer::handle_accepted, this, std::placeholders::_1));
     }
 
-  private:
-    rclcpp_action::Server<Fibonacci>::SharedPtr action_server_;
+private:
+    rclcpp_action::Server<Rotate>::SharedPtr action_server_;
 
-    rclcpp_action::GoalResponse handle_goal(
-        const rclcpp_action::GoalUUID &uuid,
-        std::shared_ptr<const Fibonacci::Goal> goal)
+    // 当收到新的动作目标时被调用。它接受目标ID和共享的目标数据，并打印旋转角度信息
+    rclcpp_action::GoalResponse handle_goal(const rclcpp_action::GoalUUID &, std::shared_ptr<const Rotate::Goal> goal)
     {
-      RCLCPP_INFO(this->get_logger(), "Received goal request with order %d", goal->order);
-      (void)uuid;
-      return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+        RCLCPP_INFO(get_logger(), "Received goal request: %d degrees", goal->goal);
+        return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+    }
+    // 处理取消动作请求。当接收到取消请求时，打印消息并返回接受取消的响应。
+    rclcpp_action::CancelResponse handle_cancel(const std::shared_ptr<GoalHandleRotate>)
+    {
+        RCLCPP_INFO(get_logger(), "Received request to cancel goal");
+        return rclcpp_action::CancelResponse::ACCEPT;
     }
 
-    rclcpp_action::CancelResponse handle_cancel(
-        const std::shared_ptr<GoalHandleFibonacci> goal_handle)
+    // 在目标被接受后调用，启动一个新的线程来执行动作任务，传入目标句柄。
+    void handle_accepted(const std::shared_ptr<GoalHandleRotate> goal_handle)
     {
-      RCLCPP_INFO(this->get_logger(), "Received request to cancel goal");
-      (void)goal_handle;
-      return rclcpp_action::CancelResponse::ACCEPT;
+        std::thread{std::bind(&RotateActionServer::execute, this, std::placeholders::_1), goal_handle}.detach();
     }
 
-    void handle_accepted(const std::shared_ptr<GoalHandleFibonacci> goal_handle)
+    // 这是动作执行的核心逻辑。在新线程中运行，负责实际的旋转操作模拟
+    void execute(const std::shared_ptr<GoalHandleRotate> goal_handle)
     {
-      using namespace std::placeholders;
-      // this needs to return quickly to avoid blocking the executor, so spin up a new thread
-      std::thread{std::bind(&FibonacciActionServer::execute, this, _1), goal_handle}.detach();
-    }
+        auto goal = goal_handle->get_goal();
+        auto feedback = std::make_shared<Rotate::Feedback>();
+        auto &current_angle = feedback->feedback;
 
-    void execute(const std::shared_ptr<GoalHandleFibonacci> goal_handle)
-    {
-      RCLCPP_INFO(this->get_logger(), "Executing goal");
-      rclcpp::Rate loop_rate(1);
-      const auto goal = goal_handle->get_goal();
-      auto feedback = std::make_shared<Fibonacci::Feedback>();
-      auto &sequence = feedback->partial_sequence;
-      sequence.push_back(0);
-      sequence.push_back(1);
-      auto result = std::make_shared<Fibonacci::Result>();
+        // 初始化当前角度为0
+        current_angle = 0;
 
-      for (int i = 1; (i < goal->order) && rclcpp::ok(); ++i)
-      {
-        // Check if there is a cancel request
-        if (goal_handle->is_canceling())
-        {
-          result->sequence = sequence;
-          goal_handle->canceled(result);
-          RCLCPP_INFO(this->get_logger(), "Goal canceled");
-          return;
-        }
-        // Update sequence
-        sequence.push_back(sequence[i] + sequence[i - 1]);
-        // Publish feedback
+        // 发布初始反馈
         goal_handle->publish_feedback(feedback);
-        RCLCPP_INFO(this->get_logger(), "Publish feedback");
 
-        loop_rate.sleep();
-      }
+        // 控制客户端旋转，每0.01秒发布一次当前角度
+        rclcpp::Rate loop_rate(100);
+        while (rclcpp::ok() && current_angle <= 270)
+        {
+            if (goal_handle->is_canceling())
+            {
+                auto result_msg = std::make_shared<action_pkg::action::Rotate::Result>();
+                goal_handle->canceled(result_msg);
+                RCLCPP_INFO(get_logger(), "Goal canceled");
+                return;
+            }
 
-      // Check if goal is done
-      if (rclcpp::ok())
-      {
-        result->sequence = sequence;
-        goal_handle->succeed(result);
-        RCLCPP_INFO(this->get_logger(), "Goal succeeded");
-      }
+            // 增加角度并更新反馈
+            current_angle++;
+
+            // // 添加日志打印当前角度变化
+            // RCLCPP_INFO(get_logger(), "Current angle: %d degrees", current_angle);
+
+            feedback->feedback = current_angle;
+            // 发布反馈
+            goal_handle->publish_feedback(feedback);
+            // 等待0.01秒
+            loop_rate.sleep();
+        }
+
+        // 检查是否完成目标
+        if (current_angle > 270)
+        {
+            auto result = std::make_shared<Rotate::Result>();
+            result->result = current_angle;
+            goal_handle->succeed(result);
+            RCLCPP_INFO(get_logger(), "Server Goal succeeded, final angle: %d degrees", current_angle);
+        }
     }
-  }; // class FibonacciActionServer
-
-} // namespace action_tutorials_cpp
-
-RCLCPP_COMPONENTS_REGISTER_NODE(action_tutorials_cpp::FibonacciActionServer)
+};
 
 int main(int argc, char **argv)
 {
-  rclcpp::init(argc, argv);
-  auto node = std::make_shared<action_tutorials_cpp::FibonacciActionServer>();
-  rclcpp::spin(node);
-  rclcpp::shutdown();
-
-  return 0;
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<RotateActionServer>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
 }
